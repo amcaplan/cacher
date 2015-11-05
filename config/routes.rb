b10fede816e812b8dffdfc06b4b565392b80f065
amcaplan <ariel.caplan@vitals.com>
2015-11-05 20:29:38 +0200

    Mount cacher engine within forget
    This depends on forget being in the same place as cacher, e.g.
    
    $ ls
    cacher       forget

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  mount Forget::Engine => 'sparkly_dinosaurs'
+
   # You can have the root of your site routed with "root"
   root 'numbers#index'
 
